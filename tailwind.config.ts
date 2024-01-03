485ecb660fff25ba7520d72285211086dc0506ea
imFaizanAhmed <imFaizanAhmed6@gmail.com>
2024-01-04 02:37:01 +0500

    App layout is created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      maxWidth: {
+        'std-width': '800px'
+      }
     },
   },
   plugins: [],
