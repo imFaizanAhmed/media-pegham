a53292791e2e40b26998f1a2bec8a55aefd36eac
imFaizanAhmed <imFaizanAhmed6@gmail.com>
2024-01-05 14:51:13 +0500

    shadcn ui integrated with theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ModeToggle } from '@/components/ui/mode-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,10 @@ export default function RootLayout({
         <div className='flex items-center justify-center'>
           <div className='flex items-end justify-between mb-8 border-b border-gray-600 w-full max-w-std-width'>
             <div className='p-8 text-3xl'> MediaPegham</div>
+            <div className='flex items-center'>
             <div className='p-8 text-base'> Login</div>
+            <ModeToggle />
+            </div>
           </div>
         </div>
         {children}
---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+import { Button } from "./button";
+import { Checkbox } from "./checkbox";
+import { Select } from "./select";
+import { Textarea } from "./textarea";
+
+export {
+    Button, Checkbox, Select, Textarea
+}
